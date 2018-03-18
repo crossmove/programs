887c41aec291e1ef357e44fb59cb187078e17bc6
pradeep <pradeep.exe@gmail.com>
2018-03-18 11:44:43 +0530

    Add project files.

---
+++ General/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace General
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DebugClass.method();
+
+           // TypeOfvsGetType typeOfObj = new TypeOfvsGetType();
+           // typeOfObj.Method();
+
+           // ISvsAs.Method();
+
+           // CoalescingOperator.Method();
+
+            //ConsoleChangeColor.Method();
+
+            //PreFixPostFix.method();
+
+           // CheckedUnchecked.Checked();
+           //CheckedUnchecked.UnChecked();
+
+           // ParamsKeyword.Driver();
+
+           // Shadowing.Driver();
+
+           // VolatileClass.Method();
+
+            //RegEx_Class.method();
+            RegEx_Class.RemoveDigits();
+            
+            //JaggedArray.Method1();
+
+           // SortLinq.Method();
+
+           // ProcessRunAsAdmin.Run();
+
+           // SwitchCaseExample.method();
+        }
+    }
+}
